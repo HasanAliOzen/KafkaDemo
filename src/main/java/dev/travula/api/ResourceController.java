5bad4977477215071deaf8c20d37ff21cbbaee32
HasanAliOzen <hasanali.ozen@outlook.com>
2024-01-04 08:12:34 +0300

    WebSocket

--- src/main/java/dev/travula/api/ResourceController.java
+++ src/main/java/dev/travula/api/ResourceController.java
@@ -3,29 +3,50 @@ package dev.travula.api;
 import dev.travula.dto.KMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
+@CrossOrigin
 public class ResourceController {
 
     @Value("${kafka.topic}")
     private String kafkaTopic;
 
-    private final KafkaTemplate<String, KMessage> kafkaTemplete;
+    private final KafkaTemplate<String, KMessage> kafkaTemplate;
 
     @PostMapping("/message")
-    public void sendMessage(@RequestBody KMessage message) {
-        kafkaTemplete.send(kafkaTopic, UUID.randomUUID().toString(),message);
-
+    public ResponseEntity<Void> sendMessage(@RequestBody KMessage message) {
+        kafkaTemplate.send(kafkaTopic, UUID.randomUUID().toString(),message);
+        return ResponseEntity.noContent().build();
     }
 
+    @MessageMapping("/send")
+    public KMessage receiveMessage(@Payload KMessage message){
+        //Sending this message to all the subscribers
+        kafkaTemplate.send(kafkaTopic, UUID.randomUUID().toString(),message);
+        return message;
+    }
 
+    @MessageMapping("/newUser")
+    @SendTo("/topic/kafka")
+    public KMessage addUser(@Payload KMessage message, SimpMessageHeaderAccessor headerAccessor){
+        // Add user in web socket session
+        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", message.sender());
+        return message;
+    }
 }
--- src/main/java/dev/travula/service/KafkaListenerService.java
+++ src/main/java/dev/travula/service/KafkaListenerService.java
@@ -1,17 +1,23 @@
 package dev.travula.service;
 
 import dev.travula.dto.KMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaListenerService {
+
+    private final SimpMessagingTemplate messagingTemplate;
 
     @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.group}")
     public void listen(@Payload KMessage message) {
         log.info("Received Message : " + message);
+        messagingTemplate.convertAndSend("/topic/kafka", message);
     }
 }
